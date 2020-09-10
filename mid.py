c6b6b1fa505efd2a17c186a0486e377b5fa7ad9e
Dan Fitch <dgfitch@gmail.com>
2020-09-10 12:33:58 -0500

    Fail on early presses

--- mid.py
+++ mid.py
@@ -506,6 +506,10 @@ for run in range(0, num_runs):
         if DEBUG:
             print('time after target: ', trialClock.getTime())
 
+        if too_fast_rt and trial_response:
+            # Fail them if they are button mashing too early
+            trial_response = 0
+
         # add the data to the current staircase so it can be used to calculate the next level
         trial_stairs.addResponse(trial_response)
         exp.addData("trial.response", trial_response)
